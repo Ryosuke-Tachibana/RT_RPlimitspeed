8dd6cfb17c754769392937d315e89bfa5cea7ff6
Ryosuke-Tachibana <kouske419@gmail.com>
2025-06-03 17:50:38 +0900

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,12 @@
+return {
+    speed_limit_setting = "Speed Limit Setting",
+    change_speed = "↑/↓: Change  Enter: Confirm",
+    speed_limit_set = "Speed limit set to %.1f km/h.",
+    speed_limit_disabled = "Speed limit disabled.",
+    notification_type_set = 'Notification type set to "%s".',
+    invalid_notification_type = 'Invalid notification type. Available types: okok, ox, qb, gta',
+    usage_setnotify = 'Usage: /setnotify [okok/ox/qb/gta]',
+    language_set = 'Language set to "%s".',
+    invalid_language = 'Invalid language. Available languages: jp, en',
+    usage_setlang = 'Usage: /setlang [jp/en]',
+}
