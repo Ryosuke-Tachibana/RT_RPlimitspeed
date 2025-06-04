34aa70b8816f78aaaa30f863c6080fe2509fe041
Ryosuke-Tachibana <kouske419@gmail.com>
2025-06-05 00:54:49 +0900

    Update jp.lua

--- locals/jp.lua
+++ locals/jp.lua
@@ -1,12 +1,13 @@
-return {
-    speed_limit_setting = "速度制限設定",
-    change_speed = "↑/↓: 変更  Enter: 決定",
-    speed_limit_set = "速度制限を %.1f km/h に設定しました。",
-    speed_limit_disabled = "速度制限を無効にしました。",
-    notification_type_set = '通知タイプを "%s" に設定しました。',
-    invalid_notification_type = '無効な通知タイプです。使用可能なタイプ: okok, ox, qb, gta',
-    usage_setnotify = '使用方法: /setnotify [okok/ox/qb/gta]',
-    language_set = '言語を "%s" に設定しました。',
-    invalid_language = '無効な言語です。使用可能な言語: jp, en',
-    usage_setlang = '使用方法: /setlang [jp/en]',
+RPlimitspeedLocale = RPlimitspeedLocale or {} 
+RPlimitspeedLocale.jp = {
+    ['speed_limit_setting'] = '速度制限設定',
+    ['change_speed'] = '上/下矢印で変更, Enterで決定',
+    ['speed_limit_set'] = '速度制限が %s km/h に設定されました',
+    ['speed_limit_disabled'] = '速度制限が無効になりました',
+    ['notification_type_set'] = '通知タイプが %s に設定されました',
+    ['invalid_notification_type'] = '無効な通知タイプです。(okok, ox, qb, gta)',
+    ['usage_setnotify'] = '/setnotify [タイプ]',
+    ['language_set'] = '言語が %s に設定されました',
+    ['invalid_language'] = '無効な言語です。(jp, en)',
+    ['usage_setlang'] = '/setlang [言語]',
 }
