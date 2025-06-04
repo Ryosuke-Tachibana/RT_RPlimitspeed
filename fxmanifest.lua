1bd70a90b8e7ed526f97853bcdd184ae396fc8aa
Ryosuke-Tachibana <kouske419@gmail.com>
2025-06-05 00:54:27 +0900

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,19 +4,21 @@ game 'gta5'
 author 'R tachibana'
 description 'RPlimitspeed allows you to set speed limits for your own vehicle.'
 
-client_script {
-    'client.lua',
-}
 
 shared_script {
     'locales/*.lua', 
 }
 
+
+client_script {
+    'client.lua',
+}
+
 files {
-    'locales/en.lua', 
-    'locales/jp.lua', 
+    'locales/en.lua',
+    'locales/jp.lua',
 }
 
 data_files {
-    ['locales'] = 'locales/*', 
+    ['locales'] = 'locales/*',
 }
