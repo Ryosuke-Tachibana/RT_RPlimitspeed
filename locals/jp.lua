8813565dcddbc8f328902ce14a300080759850c8
Ryosuke-Tachibana <kouske419@gmail.com>
2025-06-05 00:48:11 +0900

    Create jp.lua

---
+++ locals/jp.lua
@@ -0,0 +1,12 @@
+return {
+    speed_limit_setting = "速度制限設定",
+    change_speed = "↑/↓: 変更  Enter: 決定",
+    speed_limit_set = "速度制限を %.1f km/h に設定しました。",
+    speed_limit_disabled = "速度制限を無効にしました。",
+    notification_type_set = '通知タイプを "%s" に設定しました。',
+    invalid_notification_type = '無効な通知タイプです。使用可能なタイプ: okok, ox, qb, gta',
+    usage_setnotify = '使用方法: /setnotify [okok/ox/qb/gta]',
+    language_set = '言語を "%s" に設定しました。',
+    invalid_language = '無効な言語です。使用可能な言語: jp, en',
+    usage_setlang = '使用方法: /setlang [jp/en]',
+}
