0221c21f14359b476b3037fbbf2e37fc2035b99b
Ryosuke-Tachibana <kouske419@gmail.com>
2025-06-05 00:55:05 +0900

    Update en.lua

--- locals/en.lua
+++ locals/en.lua
@@ -1,12 +1,13 @@
-return {
-    speed_limit_setting = "Speed Limit Setting",
-    change_speed = "↑/↓: Change  Enter: Confirm",
-    speed_limit_set = "Speed limit set to %.1f km/h.",
-    speed_limit_disabled = "Speed limit disabled.",
-    notification_type_set = 'Notification type set to "%s".',
-    invalid_notification_type = 'Invalid notification type. Available types: okok, ox, qb, gta',
-    usage_setnotify = 'Usage: /setnotify [okok/ox/qb/gta]',
-    language_set = 'Language set to "%s".',
-    invalid_language = 'Invalid language. Available languages: jp, en',
-    usage_setlang = 'Usage: /setlang [jp/en]',
+RPlimitspeedLocale = RPlimitspeedLocale or {} 
+RPlimitspeedLocale.en = {
+    ['speed_limit_setting'] = 'Speed Limit Setting',
+    ['change_speed'] = 'Up/Down to change, Enter to confirm',
+    ['speed_limit_set'] = 'Speed limit set to %s km/h',
+    ['speed_limit_disabled'] = 'Speed limit disabled',
+    ['notification_type_set'] = 'Notification type set to %s',
+    ['invalid_notification_type'] = 'Invalid notification type. (okok, ox, qb, gta)',
+    ['usage_setnotify'] = '/setnotify [type]',
+    ['language_set'] = 'Language set to %s',
+    ['invalid_language'] = 'Invalid language. (jp, en)',
+    ['usage_setlang'] = '/setlang [lang]',
 }
